8765161981a9537cefa7d3e23a28d62d8d087e50
Tássyo Santana <tassyo.santana@hotmail.com>
2016-12-15 10:52:18 -0200

    Inicial

---
+++ src/NovatecEnergyWeb/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NovatecEnergyWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login()
+        {
+            //criar lógica de login
+            return RedirectToAction("Index");
+        }
+    }
+}
