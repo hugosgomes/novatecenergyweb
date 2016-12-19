6428ba23c16c0e67206aac710a02ef5a4f8c15e4
Tássyo Santana <tassyo.santana@hotmail.com>
2016-12-19 14:42:00 -0200

    Adicionado filtro de autenticação

--- src/NovatecEnergyWeb/Controllers/HomeController.cs
+++ src/NovatecEnergyWeb/Controllers/HomeController.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NovatecEnergyWeb.Filters.ActionFilters;
 
 namespace NovatecEnergyWeb.Controllers
 {
     public class HomeController : Controller
     {
+        [AutenticacaoFilter]
         public IActionResult Index()
         {
             return View();
         }
 
+        [AutenticacaoFilter]
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
---
+++ src/NovatecEnergyWeb/Filters/ActionFilters/AutenticacaoFilter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace NovatecEnergyWeb.Filters.ActionFilters
+{
+    public class AutenticacaoFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            object user = context.HttpContext.Session.GetInt32("FuncionarioId");
+
+            if (user != null)
+            {
+                base.OnActionExecuting(context);
+            }
+            else
+            {
+                context.Result = new RedirectToRouteResult(new RouteValueDictionary(new
+                {
+                    controller = "Account",
+                    action = "Login"
+                }));
+            }
+
+            
+        }
+    }
+}
