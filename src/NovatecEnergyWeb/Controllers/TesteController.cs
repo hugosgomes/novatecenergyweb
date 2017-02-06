933240e6cdb18d6faed1fb4d8bdf15b2e687e8f6
Tássyo Santana <tassyo.santana@hotmail.com>
2017-02-06 10:35:32 -0200

    Adicionada a classe ExportadorDados para Exportação de Excel e instalada a biblioteca EPPlus.Core

---
+++ src/NovatecEnergyWeb/Controllers/TesteController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NovatecEnergyWeb.Models;
+using Microsoft.AspNetCore.Hosting;
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NovatecEnergyWeb.Controllers
+{
+    public class TesteController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public TesteController (IHostingEnvironment hostingEnviroment)
+        {
+            _hostingEnvironment = hostingEnviroment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public FileResult ExportaExcel()
+        {
+            ExportadorDados eD = new ExportadorDados(_hostingEnvironment);
+            byte[] fileBytes = eD.ExportarParaExcel();
+            return File(fileBytes, "application/x-msdownload", eD.SFileName);
+        }
+    }
+}
