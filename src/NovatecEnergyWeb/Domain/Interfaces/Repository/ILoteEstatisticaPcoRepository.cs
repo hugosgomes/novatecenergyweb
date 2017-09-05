8a4274fe974e6f075049c5e91217dde0c5360cc5
Jonhy Jorge Guimarães Lopes <jlopes@novatecenergy.com.br>
2017-09-05 16:37:24 -0300

    implementando LoteEstatisticaPcoRepository

---
+++ src/NovatecEnergyWeb/Domain/Interfaces/Repository/ILoteEstatisticaPcoRepository.cs
@@ -0,0 +1,13 @@
+﻿using NovatecEnergyWeb.Models.StoredProcedures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NovatecEnergyWeb.Domain.Interfaces.Repository
+{
+    public interface ILoteEstatisticaPcoRepository
+    {
+        List<LoteEstatisticaPco> GetLoteEstatisticaPcoByLote(int idLote);
+    }
+}
